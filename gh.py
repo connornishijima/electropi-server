d1680733c304f499364b3e23fff5cfbc2fee7f0a
root <root@raspberrypi.(none)>
2014-12-08 12:35:34 -0700

    Added gh.py for easy commits.

---
+++ gh.py
@@ -0,0 +1,8 @@
+import os
+
+os.system("sudo git add .")
+print "What has changed in this commit?"
+commit = raw_input()
+os.system('sudo git commit -m "' + str(commit) + '"')
+os.system("sudo git push -u origin master")
+print "Done."
