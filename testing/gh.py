04cdb334fc7d8e9fac8fd3a76e7d3cce1c2d513a
root <root@raspberrypi.(none)>
2014-12-14 23:22:26 -0700

    ergrg

--- testing/gh.py
+++ testing/gh.py
@@ -25,5 +25,5 @@ os.system("sudo git add -A")
 print "What has changed in this commit?"
 commit = raw_input()
 os.system('sudo git commit -m "' + str(commit) + '"')
-os.system("sudo git push -u origin master")
+os.system("sudo git push -u origin ")
 print "Done."
